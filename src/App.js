9bda8bd466d7e35c5de0632829ccc523e1018823
dracomemnon <dumshiopoke@gmail.com>
2023-09-01 03:03:23 -0400

    All edited files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//Code to import Budget.js
+import Budget from './components/Budget';
+
+// Add code to import the other components here under
 
 import { AppProvider } from './context/AppContext';
-import Budget from './components/Budget';
+import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+import Currency from "./components/Currency";
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
+                <div className='row mt-3'>
+                    {/* Budget component */}
                     <div className='col-sm'>
                         <Budget />
                     </div>
+
+                    {/* //Remaining component */}
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <Remaining />
                     </div>
+
+                    {/* //ExpenseTotal component */}
                     <div className='col-sm'>
                         <ExpenseTotal />
-                    </div> 
+                    </div>
+                    {/* //ExpenseTotal component */}
+                    <div className='col-sm'>
+                        <Currency/>
+                    </div>
+
                     <h3 className='mt-3'>Allocation</h3>
-                    <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                        </div>
-                    </div>
-                    <h3 className='mt-3'> Change allocation</h3>
                     <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <AllocationForm/>
+                        <div className='col-sm'>
+                            <ExpenseList />
                         </div>
                     </div>
+
+                    <h3 className='mt-3'>Change allocation</h3>
+                    <div className='row mt-3'>
+                        <div className='col-sm'>
+                        <AllocationForm />
+                        </div>
+                    </div>                    
+
                 </div>
             </div>
         </AppProvider>
     );
 };
-
 export default App;
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,25 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch, currency } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const handleEditBudget = (e)=>{
+        if(e.target.value<totalExpenses) alert("Budget cannot be less than spent")
+        else if (e.target.value> 20000) alert("Budet cannot exceed 20000")
+        else dispatch({type: "SET_BUDGET", payload: e.target.value})        
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span><input
+             type="number"
+             step={10} 
+             value={budget} 
+             onChange={handleEditBudget}/>
         </div>
     );
 };
